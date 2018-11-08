9b7ed42a4e21b8b1f18d4b9af72709dc479987c3
Ilya <parshin@users.noreply.github.com>
2018-11-08 17:21:42 +0500

    week4 part 1 django templates

--- views.py
+++ views.py
@@ -1,57 +1,63 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+from django.template import loader
 
 
 @csrf_exempt
-def simple_route(request):
-    if request.method == 'GET':
-        return HttpResponse('')
-    elif request.method == 'POST' or request.method == 'PUT':
-        return HttpResponse('POST', status='405')
+def echo(request):
+
+    par_name = ''
+    par_value = ''
+    statement = 'statement is empty'
+
+    method = request.method
+    if method == 'GET':
+        if request.GET.get('a', ''):
+            par_name = 'a:'
+            par_value = request.GET['a']
+        if request.GET.get('c', ''):
+            par_name = 'c:'
+            par_value = request.GET['c']
+
+    if method == 'POST':
+        if request.POST.get('b', ''):
+            par_name = 'b:'
+            print(request.POST['b'])
+            par_value = request.POST['b']
+        if request.POST.get('d', ''):
+            par_name = 'd:'
+            par_value = request.POST['d']
+
+        if 'HTTP_X_PRINT_STATEMENT' in request.META:
+            statement = 'statement is ' + request.META['HTTP_X_PRINT_STATEMENT']
+
+    if par_name == '':
+        method = ''
+        par_name = ''
+        par_value = ''
+
+    template = loader.get_template('echo.html')
+
+    context = {
+        'method': method,
+        'par_name': par_name,
+        'par_value': par_value,
+        'statement': statement,
+    }
+
+    return HttpResponse(template.render(context, request), status='200')
 
 
-@csrf_exempt
-def slug_route(request, slug):
-    return HttpResponse(slug)
+def filters(request):
+    return render(request, 'filters.html', context={
+        'a': request.GET.get('a', 1),
+        'b': request.GET.get('b', 1)
+    })
 
 
-@csrf_exempt
-def sum_route(request , digit1, digit2):
-    return HttpResponse(int(digit1) + int(digit2))
-
-
-@csrf_exempt
-def sum_get_method(request):
-    if request.method == 'POST':
-        return HttpResponse('', status='405')
-    else:
-        a = request.GET.get('a', '')
-        b = request.GET.get('b', '')
-
-        try:
-            a, b = int(a), int(b)
-        except:
-            return HttpResponse('', status='400')
-
-        return HttpResponse(a+b)
-
-
-@csrf_exempt
-def sum_post_method(request):
-    if request.method == 'GET':
-        return HttpResponse('', status='405')
-    else:
-        a = request.POST.get('a', '')
-        b = request.POST.get('b', '')
-
-        try:
-            a, b = int(a), int(b)
-        except:
-            return HttpResponse('', status='400')
-
-        return HttpResponse(a+b)
-
+def extend(request):
+    return render(request, 'extend.html', context={
+        'a': request.GET.get('a'),
+        'b': request.GET.get('b')
+    })
